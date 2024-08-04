f7d29017c7393ddbd2c7e98e31dd8d2d2885f837
Wesley Coutinho <117856549+sousaawesley@users.noreply.github.com>
2024-08-04 18:07:21 -0300

    Delete script.bat

--- script.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-cd "D:\faculdade\5° período\Respondidos\Redes\RDT\RDT 3.0 CODE"
-start cmd /k "echo Pressione Enter para executar o Server_menu.py && pause && python Server_menu.py"
-start cmd /k "echo Pressione Enter para executar o Sender_process.py && pause && python Sender_process.py"
-start cmd /k "echo Pressione Enter para executar o Receiver_rdt.py && pause && python Receiver_rdt.py"
